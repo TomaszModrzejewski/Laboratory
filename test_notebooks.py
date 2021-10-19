905f3b013f398ac3ca5d6b247f961df1ec8593af
TomaszModrzejewski <Goh15@wp.pl>
2021-10-20 00:24:20 +0200

    test

---
+++ test_notebooks.py
@@ -0,0 +1,111 @@
+import unittest
+import sys
+import os
+import subprocess
+try:
+    import jupyter
+    jupyter_available = True
+except:
+    jupyter_available = False
+try:
+    import pandas
+    pandas_available = True
+except:
+    pandas_available = False
+try:
+    import networkx
+    networkx_available = True
+except:
+    networkx_available = False
+
+timeout=120
+
+requires_pandas = set(['max_flow_interdict', 'min_cost_flow_interdict', 'multi_commodity_flow_interdict', 'sp_interdict', 'min_cost_flow', 'mst'])
+requires_networkx = set(['mst'])
+
+
+# Testing for the notebooks - use nbconvert to execute all cells of the
+# notebook
+
+# For testing on TravisCI, be sure to include a requirements.txt that 
+# includes jupyter so that you run on the most up-to-date version. 
+
+
+# Where are the notebooks?
+TESTDIR = os.path.dirname(os.path.abspath(__file__))
+#NBDIR = os.path.sep.join(TESTDIR.split(os.path.sep)[:-2] + ['notebooks/']) # where are the notebooks?
+
+def setUp():
+    nbpaths = []  # list of notebooks, with file paths
+    nbnames = []  # list of notebook names (for making the tests)
+
+    print(TESTDIR)
+    # walk the test directory and find all notebooks
+    for dirname, dirnames, filenames in os.walk(TESTDIR):
+        for filename in filenames:
+            if filename.endswith('.ipynb') and not filename.endswith('-checkpoint.ipynb'):
+                nbpaths.append(os.path.abspath(dirname) + os.path.sep + filename) # get abspath of notebook
+                nbnames.append(''.join(filename[:-6])) # strip off the file extension
+    return nbpaths, nbnames
+
+
+def get(nbname, nbpath):
+
+    # use nbconvert to execute the notebook
+    def test_func(self):
+        print('\n--------------- Testing {0} ---------------'.format(nbname))
+        print('   {0}'.format(nbpath))
+        if not jupyter_available:
+            self.skipTest("Jupyter unavailable")
+        if nbname in requires_pandas and not pandas_available:
+            self.skipTest("Pandas unavailable")
+        if nbname in requires_networkx and not networkx_available:
+            self.skipTest("Networkx unavailable")
+
+        # execute the notebook using nbconvert to generate html 
+        dir_=os.path.dirname(nbpath)
+        os.chdir(dir_)
+        nbexe = subprocess.Popen(
+            [ 'jupyter', 'nbconvert', '{0}'.format(nbpath),
+              '--execute',
+              '--inplace',
+              '--ExecutePreprocessor.kernel_name=python%s' % (
+                  {2:"",3:"3"}[sys.version_info[0]], ),
+              '--ExecutePreprocessor.timeout='+str(timeout)],
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        output, err = nbexe.communicate()
+        check = nbexe.returncode
+        if check == 0:
+            print('\n ..... {0} Passed ..... \n'.format(nbname))
+            # if passed remove the generated html file
+            #subprocess.call(['rm', '{0}.html'.format( os.path.sep.join(os.getcwd().split(os.path.sep) + [nbpath.split(os.path.sep)[-1][:-6]]))])
+        else:
+            print('\n <<<<< {0} FAILED >>>>> \n'.format(nbname))
+            print('Captured Output: \n {0}'.format(err))
+
+        self.assertEqual(check, 0)
+
+    return test_func
+
+
+class TestNotebooks(unittest.TestCase):
+    pass
+
+nbpaths, nbnames = setUp()
+# Check for duplicates
+tmp = set()
+for name in nbnames:
+    if name in tmp:
+        raise IOError("ERROR: duplicate test name %s" % name)
+    tmp.add(name)
+
+# build test for each notebook
+for i, nb in enumerate(nbnames):
+    #print((i,nb,nbpaths[i]))
+    setattr(TestNotebooks, 'test_'+nb, get(nb, nbpaths[i]))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
